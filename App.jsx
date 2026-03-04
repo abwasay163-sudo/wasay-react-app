6f37f401e1bc91469013470edb13329d46c04c4a
abwasay163-sudo <abwasay163@gmail.com>
2026-03-05 03:49:47 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,32 @@
+import Myapp from "./components/myapp"
+import First from "./components/first"
+function App() {
+  
+
+  return <center>
+    <Myapp></Myapp>
+    <First></First>
+      <First></First>
+        <First></First>
+    
+   
+
+    <div class="container text-center">
+  <div class="row">
+    <div class="col-6">
+   buy milk
+    </div>
+    <div class="col-4">
+      4/10/2023
+    </div>
+    <div class="col-2">
+      <button type="button" class="btn btn-danger">Delete</button>
+    </div>
+  </div>
+</div>
+  </center>
+  
+  
+}
+
+export default App
---
+++ myapp.jsx
@@ -0,0 +1,5 @@
+function Myapp(){
+return <h1>React todo app</h1>
+
+}
+export default Myapp;
